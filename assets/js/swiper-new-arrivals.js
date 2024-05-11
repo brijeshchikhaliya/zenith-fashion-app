b5295e7bee1fedbf8dc73c20e3d5664cdd348b5b
brijeshchikaliya <brijesh.chikaliya@oneonic.com>
2024-05-11 18:53:20 +0530

    crate responsive in max screen  576px

--- assets/js/swiper-new-arrivals.js
+++ assets/js/swiper-new-arrivals.js
@@ -8,20 +8,28 @@ const swiper = new Swiper(".new-arrivals-swiper", {
     clickable: true,
   },
   breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
     // when window width is >= 320px
-    768: {
+    767: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 25,
     },
     // when window width is >= 480px
     1024: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 25,
     },
     // when window width is >= 640px
     1280: {
       slidesPerView: 5,
-      spaceBetween: 50,
+      spaceBetween: 25,
     },
   },
 });
